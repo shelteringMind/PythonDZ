426fe42d12b5026c3197d45d999ef34c877a4911
Anton Kruglyakov <akrugs@mail.ru>
2023-05-28 03:06:31 +0500

    zadacha 20

--- dz3.py
+++ dz3.py
@@ -31,43 +31,43 @@
 #6
     #-> 5
     
-import random
-import time
+#import random
+#import time
         
-print(f"Введите количество чисел в списке:", end = ' ')
-N = int(input())
-print(f"Введите значение элемента,\nсамый близкий по величине элемент к которому требуется найти:", end = ' ')
-X = int(input())
+#print(f"Введите количество чисел в списке:", end = ' ')
+#N = int(input())
+#print(f"Введите значение элемента,\nсамый близкий по величине элемент к которому требуется найти:", end = ' ')
+#X = int(input())
 
-sp = [random.randint(1, 100) for i in range(N)]
+#sp = [random.randint(1, 100) for i in range(N)]
 
-start_set = time.time_ns()        
-sp_new =list(set(sp))        
-val_comp = abs(X-sp_new[0])
-val_save = sp_new[0]        
-for val in sp_new:
-    if abs(val-X) < val_comp:
-        val_save = val
-        val_comp = abs(X-val)       
-end_set = time.time_ns() - start_set
+#start_set = time.time_ns()        
+#sp_new =list(set(sp))        
+#val_comp = abs(X-sp_new[0])
+#val_save = sp_new[0]        
+#for val in sp_new:
+    #if abs(val-X) < val_comp:
+        #val_save = val
+        #val_comp = abs(X-val)       
+#end_set = time.time_ns() - start_set
 
-start_list = time.time_ns()
-val_comp = abs(X-sp[0])
-val_save = sp[0]
-for val in sp:
-    if abs(val-X) < val_comp:
-        val_save = val
-        val_comp = abs(X-val)
-end_list = time.time_ns() - start_list
+#start_list = time.time_ns()
+#val_comp = abs(X-sp[0])
+#val_save = sp[0]
+#for val in sp:
+    #if abs(val-X) < val_comp:
+        #val_save = val
+        #val_comp = abs(X-val)
+#end_list = time.time_ns() - start_list
 
-print(*sp, f"-> {val_save}", sep=' ')
+#print(*sp, f"-> {val_save}", sep=' ')
 
-try:
-    speed_diff = end_list/end_set
-except ZeroDivisionError:
-    speed_diff = 1
+#try:
+    #speed_diff = end_list/end_set
+#except ZeroDivisionError:
+    #speed_diff = 1
     
-print(f"Алгоритм с использованием множества в {speed_diff:.{2}f} раз быстрее")
+#print(f"Алгоритм с использованием множества в {speed_diff:.{2}f} раз быстрее")
 
 
 # *Задача 20: * В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность. 
@@ -93,3 +93,26 @@ print(f"Алгоритм с использованием множества в {
 #*Пример:*
 #ноутбук
 #12
+
+one_cost = dict.fromkeys(['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'], 1)
+two_cost = dict.fromkeys(['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'], 2)
+three_cost = dict.fromkeys(['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'], 3)
+four_cost = dict.fromkeys(['F', 'H', 'V', 'W', 'Y', 'Й', 'Ы' ], 4)
+five_cost = dict.fromkeys(['K', 'Ж', 'З', 'Х', 'Ц', 'Ч' ], 5)
+eight_cost = dict.fromkeys(['J', 'X', 'Ш', 'Э', 'Ю'], 8)
+ten_cost  = dict.fromkeys(['Q', 'Z', 'Ф', 'Щ', 'Ъ'], 10)
+merged_dict = one_cost | two_cost | three_cost | four_cost | five_cost | eight_cost | ten_cost
+
+print(f"Введите слово:", end = ' ')
+str = input()
+str_print = str
+
+str = str.strip()
+str = str.upper()
+str_list = list(str)
+cost_counter = 0;
+
+for iter, letter in enumerate(str_list): 
+    cost_counter += merged_dict.get(letter, 0)
+    
+print(f"{str_print} стоит {cost_counter}")

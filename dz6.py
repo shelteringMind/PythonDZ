8a309489c1d456ef49e2acd00e8788cc71e57252
Anton Kruglyakov <akrugs@mail.ru>
2023-06-07 01:46:51 +0500

    zadacha 32

--- dz6.py
+++ dz6.py
@@ -3,12 +3,35 @@
 #Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 #Каждое число вводится с новой строки
 
-firstElement = int(input("Введите первый элемент арифметической прогрессии: "))
+#firstElement = int(input("Введите первый элемент арифметической прогрессии: "))
 
-difference = int(input("Введите разность арифметической прогрессии: "))
+#difference = int(input("Введите разность арифметической прогрессии: "))
+
+#amountElements = int(input("Введите количество элементов: "))
+
+#searchMember = [firstElement + (element - 1) * difference for element in range(1, amountElements + 1)]
+#for searchMem in searchMember:
+    #print(searchMem)
+    
+#Задача 32: Определить индексы элементов массива (списка),
+#значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+import random
 
 amountElements = int(input("Введите количество элементов: "))
 
-searchMember = [firstElement + (element - 1) * difference for element in range(1, amountElements + 1)]
-for searchMem in searchMember:
-    print(searchMem)
+listElements = [random.randint(1, 100) for val in range(amountElements)]
+
+minElement = int(input("Введите минимальный элемент: "))
+
+maxElement = int(input("Введите максимальный элемент: "))
+
+listIndex = [index for index, val in enumerate(listElements) if val >= minElement and val <= maxElement]
+
+for listEl in listElements:
+    print(f"{listEl}", end =" ")
+
+print(f"\n")
+
+for listId in listIndex:
+    print(f"{listId}", end =" ")
